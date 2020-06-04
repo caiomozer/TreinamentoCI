2cbe163d45677798000a6f918e660b3ebfa58a76
CAIO MOZER NUNES <caiomozer@gmail.com>
2020-06-04 18:15:47 -0300

    Primeiro commit, arquivo README

---
+++ features/home.feature
@@ -0,0 +1,6 @@
+Feature: testando home
+  I want to visit home
+   Scenario: visit home
+    Given I'm staying home
+    When I click on button
+    Then visualize something
---
+++ features/home_steps.js
@@ -0,0 +1,16 @@
+const { Given, When, Then } = require("cucumber");
+
+Given('I\'m staying home', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'sucess';
+});
+
+Then('visualize something', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'sucess';
+});
+
+When('I click on button', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'sucess';
+});
